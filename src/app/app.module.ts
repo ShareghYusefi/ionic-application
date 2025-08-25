35c6daa879c0644b08f859cb8629745d28fcd866
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-08-25 17:41:01 -0600

    add task form inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,12 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
