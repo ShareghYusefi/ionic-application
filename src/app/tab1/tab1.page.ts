165172e06ea6b604cc4e43b502f07f32dda87b15
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-08-25 17:06:02 -0600

    FAB button, ionic components, angular routing

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,7 +8,9 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab1Page {
+  constructor(private navCtrl: NavController) {}
 
-  constructor() {}
-
+  openForm() {
+    this.navCtrl.navigateForward('/tabs/task-form');
+  }
 }
