a972097baf7ff79082c09172dce414cf089ce042
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-08-26 17:27:08 -0600

    tasks list UI, ionic lifcycle hooks

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Task } from '../interfaces/task';
+import { TaskService } from '../services/task-service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,8 +9,25 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss'],
   standalone: false,
 })
-export class Tab1Page {
-  constructor(private navCtrl: NavController) {}
+export class Tab1Page implements OnInit {
+  tasks!: Task[];
+
+  constructor(private navCtrl: NavController, private service: TaskService) {}
+
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  
+  ionViewDidEnter() {
+    this.loadTasks();
+  }
+
+  loadTasks() {
+    this.service.gettasks().subscribe((response) => {
+      this.tasks = response;
+    });
+  }
 
   openForm() {
     this.navCtrl.navigateForward('/tabs/task-form');
