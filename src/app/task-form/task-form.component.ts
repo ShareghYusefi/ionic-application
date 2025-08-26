997692741a31157ccfc0be557be1bb3dd4b35152
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-08-26 17:03:29 -0600

    reactive form validation

--- src/app/task-form/task-form.component.ts
+++ src/app/task-form/task-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { TaskService } from '../services/task-service';
 import { Task } from '../interfaces/task';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
@@ -10,23 +11,47 @@ import { Task } from '../interfaces/task';
   standalone: false,
 })
 export class TaskFormComponent implements OnInit {
-  task: Task = {
-    id: 0,
-    title: '',
-    description: '',
-    priority_level: '',
-  };
+  task!: FormGroup;
 
-  constructor(private navCtrl: NavController, private service: TaskService) {}
+  // Task = {
+  //   id: 0,
+  //   title: '',
+  //   description: '',
+  //   priority_level: '',
+  // };
 
-  ngOnInit() {}
+  constructor(
+    private navCtrl: NavController,
+    private service: TaskService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit() {
+    this.task = this.formBuilder.group({
+      id: [0, [Validators.required]],
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      description: ['', [Validators.required]],
+      priority_level: ['', [Validators.required]],
+    });
+  }
+
+  // getters for displaying errors in html file
+  get title() {
+    return this.task.get('title');
+  }
+  get description() {
+    return this.task.get('description');
+  }
+  get priority_level() {
+    return this.task.get('priority_level');
+  }
 
   closeForm() {
     this.navCtrl.navigateRoot('/tabs/tab1');
   }
 
   onSubmit() {
-    this.service.addtask(this.task).subscribe(
+    this.service.addtask(this.task.value).subscribe(
       (response) => {
         console.log('Task added!', response);
         this.navCtrl.navigateBack('tabs/tab1');
