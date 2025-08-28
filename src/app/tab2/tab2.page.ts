04cf2183ca1a91771ca4458574d03952d70a00bf
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-08-27 19:16:19 -0600

    email composer

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {
+  EmailComposer,
+  HasAccountResult,
+  OpenOptions,
+} from 'capacitor-email-composer';
 
 @Component({
   selector: 'app-tab2',
@@ -12,5 +17,23 @@ export class Tab2Page {
 
   nagivateToContact() {
     this.router.navigate(['/tabs/tab3']);
+  }
+
+  openEmail() {
+    EmailComposer.hasAccount()
+      .then((result: HasAccountResult) => {
+        // if result is truthy, user has mail account setup
+        if (result) {
+          //open the email composer
+          EmailComposer.open({
+            to: ['sharegh.yusefi@robogarden.ca'],
+            subject: 'Email Button clicked!',
+            body: 'This is a test email from the Ionic appliaction.',
+          });
+        }
+      })
+      .catch((error) => {
+        console.error('Error checking email account: ', error);
+      });
   }
 }
